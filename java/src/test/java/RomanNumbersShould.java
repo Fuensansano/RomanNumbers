49d004fa7c478ee1e278704f088bfbdbaa2d82aa
FuenSansano <i.fuensanosano@gmail.com>
2022-05-09 10:37:21 +0200

    change setup

--- java/src/test/java/RomanNumbersShould.java
+++ java/src/test/java/RomanNumbersShould.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class FizzBuzzTest {
+public class RomanNumbersShould {
 
 	@Test
 	public void renameMe() {

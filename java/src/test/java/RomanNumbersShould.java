a8db05fd03683e5069344793cd18ded762db6cb9
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-05-09 11:21:50 +0200

    refactor test with setup

--- java/src/test/java/RomanNumbersShould.java
+++ java/src/test/java/RomanNumbersShould.java
@@ -1,25 +1,29 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RomanNumbersShould {
 
+  public RomanNumbers romanNumbers;
+
+  @BeforeEach
+  public void setUp() {
+    romanNumbers = new RomanNumbers();
+  }
 
 	@Test
 	public void should_return_I_when_given_1() {
-    RomanNumbers romanNumbers = new RomanNumbers();
     assertEquals("I", romanNumbers.convert(1));
 	}
 
   @Test
   public void should_return_II_when_given_2() {
-    RomanNumbers romanNumbers = new RomanNumbers();
     assertEquals("II", romanNumbers.convert(2));
   }
 
   @Test
   public void should_return_III_when_given_3() {
-    RomanNumbers romanNumbers = new RomanNumbers();
     assertEquals("III", romanNumbers.convert(3));
   }
 

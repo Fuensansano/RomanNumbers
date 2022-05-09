52b621e4cbaa012da279fb6af431703b0fe03b81
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-05-09 12:16:38 +0200

    refactor names and extract to method

--- java/src/main/java/RomanNumbers.java
+++ java/src/main/java/RomanNumbers.java
@@ -1,30 +1,14 @@
 
 public class RomanNumbers {
-public final String ONE = "I";
+  public final String ONE = "I";
   public final String FIVE = "V";
+  public final String TEN = "X";
 
   public String convert(int number) {
     String result = "";
 
-    if (number == 11) {
-      return "XI";
-    }
-
-    if (number == 10) {
-      return "X";
-    }
-
     if (number == 9) {
       return "IX";
-    }
-
-    if (number > 5) {
-      number = number - 5;
-      result += FIVE;
-    }
-
-    if (number == 5) {
-      return "V";
     }
 
     if (number == 4) {
@@ -38,8 +22,19 @@ public final String ONE = "I";
 
   private String appendCharacter(int number) {
     String result = "";
+
+    if (number >= 10) {
+      number = number - 10;
+      result += TEN;
+    }
+
+    if (number >= 5) {
+      number = number - 5;
+      result += FIVE;
+    }
+
     for (int i = 0; i < number; i++) {
-      result += "I";
+      result += ONE;
     }
     return result;
   }

ba70f74e3519d73a7e368078d65e29a2da9d2608
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-05-11 12:01:52 +0200

    should_return_CD_when_given_400

--- java/src/test/java/RomanNumbersShould.java
+++ java/src/test/java/RomanNumbersShould.java
@@ -31,7 +31,8 @@ public class RomanNumbersShould {
       "XL, 40",
       "L, 50",
       "XC, 90",
-      "C, 100"
+      "C, 100",
+      "CD, 400"
   })
   public void should_return_roman_number_when_given_decimal_number(String romanNumber, int decimalNumber) {
     assertEquals(romanNumber, romanNumbers.convert(decimalNumber));

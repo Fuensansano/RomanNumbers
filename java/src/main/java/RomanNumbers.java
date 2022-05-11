b949a2f6d5ae49bea5b0eb5e544f760d66223fa4
FuenSansano <i.fuensanosano@gmail.com>
2022-05-11 11:47:48 +0200

    refactoring logical from convert method

--- java/src/main/java/RomanNumbers.java
+++ java/src/main/java/RomanNumbers.java
@@ -1,7 +1,7 @@
 public class RomanNumbers {
   public enum DecimalToRomanNumber {
     FORTY ("XL", 40),
-    TWENTY ("XX", 20),
+    //TWENTY ("XX", 20),
     TEN ("X", 10),
     NINE ("IX", 9),
     FIVE ("V", 5),
@@ -20,24 +20,24 @@ public class RomanNumbers {
   public String convert(int number) {
     String result = "";
 
-    result += appendCharacter(number);
-
-    return result;
-  }
-
-  private String appendCharacter(int number) {
-    String result = "";
-
     for (DecimalToRomanNumber decimalToRomanNumber : DecimalToRomanNumber.values()) {
 
       if (number >= decimalToRomanNumber.decimalNumber) {
-        number = number - decimalToRomanNumber.decimalNumber;
-        result += decimalToRomanNumber.romanNumber;
+        result += appendCharacter(number / decimalToRomanNumber.decimalNumber, decimalToRomanNumber.romanNumber);
+        number =  number % decimalToRomanNumber.decimalNumber;
       }
     }
 
+
+    return result;
+
+  }
+
+  private String appendCharacter(int number, String character) {
+    String result = "";
+
     for (int i = 0; i < number; i++) {
-      result += "I";
+      result += character;
     }
 
     return result;
